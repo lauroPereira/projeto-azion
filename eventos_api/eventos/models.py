0d5b6d8de003ca199a8e5ed7f31de5eeee7cbb88
lauroPereira <lauro.pereira@ampeli.com.br>
2025-04-04 09:57:54 -0300

    start django project with postgres database

---
+++ eventos_api/eventos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    data_hora = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
