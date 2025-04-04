284c1e969543890d9390fb049d6440eea61bb152
lauroPereira <lauro.pereira@ampeli.com.br>
2025-04-04 10:14:07 -0300

    feat events crud

--- eventos_api/eventos/views.py
+++ eventos_api/eventos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Evento
+from .serializers import EventoSerializer
 
+class EventoViewSet(viewsets.ModelViewSet):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 # Create your views here.
